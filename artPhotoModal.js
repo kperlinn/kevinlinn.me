b2df6df7e55a4a23b1aa4c5b67aa2b90262a59c6
Kevin Linn <32662716+kperlinn@users.noreply.github.com>
2017-11-08 12:29:14 -0800

    Add files via upload

---
+++ artPhotoModal.js
@@ -0,0 +1,83 @@
+var modal = document.getElementById('myModal');
+var img = document.getElementById('art1');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+var hide = document.getElementsByClassName("close")[0];
+myModal.onclick = function() { 
+  modal.style.display = "none";
+}
+// image 2
+var modal2 = document.getElementById('myModal2');
+var img2 = document.getElementById('art2');
+var modalImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption2");
+img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+    captionText2.innerHTML = this.alt;
+}
+var hide2 = document.getElementsByClassName("close2")[0];
+myModal2.onclick = function() { 
+  modal2.style.display = "none";
+}
+// image 3
+var modal3 = document.getElementById('myModal3');
+var img3 = document.getElementById('art3');
+var modalImg3 = document.getElementById("img03");
+var captionText3 = document.getElementById("caption3");
+img3.onclick = function(){
+    modal3.style.display = "block";
+    modalImg3.src = this.src;
+    captionText3.innerHTML = this.alt;
+}
+var hide3 = document.getElementsByClassName("close3")[0];
+myModal3.onclick = function() { 
+  modal3.style.display = "none";
+}
+// image 4
+var modal4 = document.getElementById('myModal4');
+var img4 = document.getElementById('art4');
+var modalImg4 = document.getElementById("img04");
+var captionText4 = document.getElementById("caption4");
+img4.onclick = function(){
+    modal4.style.display = "block";
+    modalImg4.src = this.src;
+    captionText4.innerHTML = this.alt;
+}
+var hide4 = document.getElementsByClassName("close4")[0];
+myModal4.onclick = function() { 
+  modal4.style.display = "none";
+}
+// image 5
+var modal5 = document.getElementById('myModal5');
+var img5 = document.getElementById('art5');
+var modalImg5 = document.getElementById("img05");
+var captionText5 = document.getElementById("caption5");
+img5.onclick = function(){
+    modal5.style.display = "block";
+    modalImg5.src = this.src;
+    captionText5.innerHTML = this.alt;
+}
+var hide5 = document.getElementsByClassName("close5")[0];
+myModal5.onclick = function() { 
+  modal5.style.display = "none";
+}
+// image 6
+var modal6 = document.getElementById('myModal6');
+var img6 = document.getElementById('art6');
+var modalImg6 = document.getElementById("img06");
+var captionText6 = document.getElementById("caption6");
+img6.onclick = function(){
+    modal6.style.display = "block";
+    modalImg6.src = this.src;
+    captionText6.innerHTML = this.alt;
+}
+var hide6 = document.getElementsByClassName("close6")[0];
+myModal6.onclick = function() { 
+  modal6.style.display = "none";
+}
